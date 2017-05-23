a42863d70be3a34c762aebe0c8e4ce1d28f474e7
nandini0407 <nandini0407@gmail.com>
2017-05-22 17:39:24 -0700

    make api call

--- number_facts.jsx
+++ number_facts.jsx
@@ -3,12 +3,49 @@ import React from 'react';
 class NumberFacts extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      num: "",
+      fact: ""
+     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      num: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'GET',
+      url: `http://numbersapi.com/${this.state.num}`
+    }).then((res) => {
+      this.setState({ fact: res });
+    });
   }
 
   render() {
     return (
       <div>
-        Hello React
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            type="text"
+            value={ this.state.num }
+            onChange={ this.handleChange }
+            />
+          <input
+            type="submit"
+            value="Get Fact"
+            />
+        </form>
+        <div>
+          { this.state.fact }
+        </div>
       </div>
     );
   }
