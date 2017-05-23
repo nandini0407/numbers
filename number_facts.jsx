bee3547e49ff9518d474bc28b138b024b02d7a71
nandini0407 <nandini0407@gmail.com>
2017-05-22 18:48:52 -0700

    add dropdown

--- number_facts.jsx
+++ number_facts.jsx
@@ -5,11 +5,13 @@ class NumberFacts extends React.Component {
     super(props);
     this.state = {
       num: "",
-      fact: ""
+      fact: "",
+      type: "Choose Fact Type"
      };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.selectOption = this.selectOption.bind(this);
   }
 
   handleChange(e) {
@@ -21,12 +23,19 @@ class NumberFacts extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    let type = this.state.type;
+    type = type === "Choose Fact Type" ? "trivia" : type;
     $.ajax({
       method: 'GET',
-      url: `http://numbersapi.com/${this.state.num}`
+      url: `http://numbersapi.com/${this.state.num}/${type}`
     }).then((res) => {
       this.setState({ fact: res });
     });
+  }
+
+  selectOption(e) {
+    e.preventDefault();
+    this.setState({ type: e.target.value });
   }
 
   render() {
@@ -39,6 +48,13 @@ class NumberFacts extends React.Component {
             onChange={ this.handleChange }
             className="text-box"
             />
+          <select value={ this.state.type } onChange={ this.selectOption } className="dropdown">
+            <option value="Choose Fact Type" className="dropdown-option">Choose fact type</option>
+            <option className="dropdown-option" value="trivia" >trivia</option>
+            <option className="dropdown-option" value="math" >math</option>
+            <option className="dropdown-option" value="date" >date</option>
+            <option className="dropdown-option" value="year" >year</option>
+          </select>
           <input
             type="submit"
             value="Get Fact"
